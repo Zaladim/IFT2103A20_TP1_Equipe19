2c72c057a748c4b03c38e73128015188c82635de
Zaladim <48721924+Zaladim@users.noreply.github.com>
2020-10-15 09:47:50 +0200

    Report
    Start of report

--- Assets/Resources/Shoot.cs
+++ Assets/Resources/Shoot.cs
@@ -6,16 +6,12 @@ public class Shoot : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject bullet;
-    public float speed = 0.01f;
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && GameObject.FindGameObjectWithTag("projectile") == null)
         {
             GameObject projectile = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
             Destroy(projectile, 5);
